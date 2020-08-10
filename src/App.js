4df0f3dc6f9e8c8cff496b727127cc53309fb945
tsengpoyuan <36426936+tsengpoyuan@users.noreply.github.com>
2020-08-10 13:49:27 -0400

    dropdown

--- src/App.js
+++ src/App.js
@@ -1,27 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 
-const items =[
-    {
-        title: 'What is React',
-        content: 'React is a front end javascript framework'
-    },
-    {
-        title: 'Why use React',
-        content: 'React is a favorite JS library among engineers'
-    },
-    {
-        title: 'How do you use React',
-        content: 'You use React by creating component',
-    }
-]
+const items = [
+  {
+    title: 'What is React',
+    content: 'React is a front end javascript framework',
+  },
+  {
+    title: 'Why use React',
+    content: 'React is a favorite JS library among engineers',
+  },
+  {
+    title: 'How do you use React',
+    content: 'You use React by creating component',
+  },
+];
 
-export default ()=>{
-    return (
-        <div>
-            <Search />
-            <Accordion items={items} />
-        </div>  
-    );
+const options = [
+  {
+    label: 'The Color Red',
+    value: 'red',
+  },
+  {
+    label: 'The color Green',
+    value: 'Green',
+  },
+  {
+    label: 'The shade of blue',
+    vlaue: 'blue',
+  },
+];
+
+export default () => {
+  const [selected, setSelected] = useState(options[0]);
+  return (
+    <div>
+      <Dropdown
+        selected={selected}
+        onSelectedChange={setSelected}
+        options={options}
+      />
+    </div>
+  );
 };
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,70 +1,71 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const Search = () =>{
-    const [term, setTerm] = useState('programming');
-    const [results, setResult] = useState([]);
+const Search = () => {
+  const [term, setTerm] = useState('programming');
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
+  const [results, setResult] = useState([]);
 
-    console.log(results);
+  useEffect(() => {
+    const timeId = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 1000);
 
-    useEffect(()=>{
-        const search = async()=>{
-            const {data} = await axios.get('https://en.wikipedia.org/w/api.php',{
-                params:{
-                    action:'query',
-                    list:'search',
-                    origin:'*',
-                    format:'json',
-                    srsearch: term
-                }
-            });
-            setResult(data.query.search);
-        };
+    return () => {
+      clearTimeout(timeId);
+    };
+  }, [term]);
 
-        const timeoutId = setTimeout(()=>{
-            if(term){
-                search();
-            }
-        }, 500);       
-    }, [term]);
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term,
+        },
+      });
+      setResult(data.query.search);
+    };
+    search();
+  }, [debouncedTerm]);
 
-    const renderedResults = results.map((result)=>{
-        return(
-            <div key={result.pageid} className="item">
-                <div className="right floated content">
-                    <a 
-                        className="ui button"
-                        href={`http://en.wikipedia.org?curid=${result.pageid}`}
-                    >
-                        Go
-                    </a>
-                </div>
-                <div className="content">
-                    <div className="header">
-                        {result.title}
-                    </div>
-                    <span dangerouslySetInnerHTML={{__html:result.snippet}}></span>                    
-                </div>
-            </div>
-        );
-    });
-
+  const renderedResults = results.map((result) => {
     return (
-        <div>
-            <div className="ui form">
-                <div className="field">
-                    <label>Enter Search Term</label>
-                    <input 
-                        value={term}
-                        onChange={(e) => setTerm(e.target.value)}
-                        className="input" />
-                </div>
-            </div>
-            <div className="ui celled list">
-                {renderedResults}
-            </div>
+      <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`http://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
         </div>
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
     );
+  });
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search Term</label>
+          <input
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+            className="input"
+          />
+        </div>
+      </div>
+      <div className="ui celled list">{renderedResults}</div>
+    </div>
+  );
 };
 
 export default Search;
